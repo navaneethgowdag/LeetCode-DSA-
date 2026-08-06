70e0118702e7e6233df96e7f6b5107b9c2f64fe2
Navaneeth Gowda G <165000822+navaneethgowdag@users.noreply.github.com>
2026-08-06 11:28:53 +0530

    Time: 35 ms (59.57%) | Memory: 13.2 MB (61.47%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -2,10 +2,9 @@ class Solution(object):
     def canJump(self, nums):
         farthest = 0
         for i in range(len(nums)):
-            if i <= farthest:
-                farthest = max(farthest, nums[i] + i)
-
             if i > farthest:
                 return False
+
+            farthest = max(farthest, nums[i] + i)
 
         return True
